638deaae21174ac404aee6a3e2cbad2c7dc79026
radiansz <lightwaveinthespace@gmail.com>
2019-02-08 10:06:54 +0300

    Connect reducer to store, use store in App

--- packages/croc-client/src/App.tsx
+++ packages/croc-client/src/App.tsx
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import createStore from './store/createStore';
 import logo from './logo.svg';
 import './App.css';
+
+const store = createStore();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
--- packages/croc-client/src/store/reducers.ts
+++ packages/croc-client/src/store/reducers.ts
@@ -1,4 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as connectionReducer } from '../modules/connections';
+import { reducer as chatReducer } from '../modules/chatMessages';
+import { reducer as answersReducer } from '../modules/answers';
+import { reducer as playersReducer } from '../modules/players';
 
 export default combineReducers({
+  connection: connectionReducer,
+  chatMessages: chatReducer,
+  answers: answersReducer,
+  players: playersReducer,
 });
